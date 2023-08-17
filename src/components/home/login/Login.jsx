10a6e028043b97cdba1054e3c7f144a9bc545f0b
Robinhood <okaforchidubem7@gmail.com>
2023-08-17 14:03:02 +0100

    connected front to backend

--- src/components/home/login/Login.jsx
+++ src/components/home/login/Login.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "../signup/signup_and_login.css"
 import { Link } from 'react-router-dom'
-
+import axios from "axios"
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  async function login () {
+   const data = {
+      email: email,
+      password: password
+    }
+    try {
+      const response = await axios.post("http://localhost:8090/api/login",data)
+      if(response.data.message == 'login successful') {
+         
+      }
+      }catch(e){
+        console.error(e)
+      }
+  }
+ 
   return (
     <div className='login_and_signup_main'>
       <div className='login_and_signup_background_image'>
@@ -17,10 +36,10 @@ const Login = () => {
             <p className='p4'>Let's log you in quickly</p>
           </div>
           <form>
-             <input type='email' placeholder='Enter your email'/>
-             <input type='password' placeholder='Enter password'/>
+             <input type='email' placeholder='Enter your email' onChange={(e) => setEmail(e.target.value)} />
+             <input type='password' placeholder='Enter password' onChange={(e) => setPassword(e.target.value)}/>
              <div className='buttondiv'>
-             <button className='submit'>LOGIN</button>
+             <button className='submit' type='button' onClick={login}>LOGIN</button>
              <div className='p1_container'><p className='p1'>don't have an account? <Link to={'/signup'}><span className='login_span'>Sign-up</span></Link></p></div>
              </div>
           </form>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import LoginComponent from '../../components/home/login/Login'
+
 const Login = () => {
+
+  
   return (
     <div>
       <LoginComponent/>
